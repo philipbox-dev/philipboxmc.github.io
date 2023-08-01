aa08c71cce0b59bd622cf22d3abce61310aa5559
Philipbox <philip.vermel@mail.ru>
2023-08-01 11:07:05 +0300

    Voting reload

--- 25566/poll_vote.php
+++ 25566/poll_vote.php
@@ -6,19 +6,25 @@ $filename = "poll_result.txt";
 $content = file($filename);
 
 //put content in array
-$array = explode("||", $content[0]);
+$array = explode("||||", $content[0]);
 $yes = $array[0];
 $no = $array[1];
 
 if ($vote == 0) {
-  $yes = $yes + 1;
+  $Матвей = $Матвей + 1;
 }
 if ($vote == 1) {
-  $no = $no + 1;
+  $Саша = $Саша + 1;
 }
+if ($vote == 2) {
+    $Гера = $Гера + 1;
+  }
+  if ($vote == 3) {
+    $Коля = $Коля + 1;
+  }
 
 //insert votes to txt file
-$insertvote = $yes."||".$no;
+$insertvote = $Матвей."||".$Саша $Гера."||".$Коля;
 $fp = fopen($filename,"w");
 fputs($fp,$insertvote);
 fclose($fp);
@@ -27,19 +33,35 @@ fclose($fp);
 <h2>Result:</h2>
 <table>
 <tr>
-<td>Yes:</td>
+<td>Матвей:</td>
 <td><img src="poll.gif"
-width='<?php echo(100*round($yes/($no+$yes),2)); ?>'
+width='<?php echo(100*round($Матвей/($Саша+$Матвей+$Гера+$Коля),2)); ?>'
 height='20'>
-<?php echo(100*round($yes/($no+$yes),2)); ?>%
+<?php echo(100*round($Матвей/($Саша+$Матвей+$Гера+$Коля),2)); ?>%
 </td>
 </tr>
 <tr>
-<td>No:</td>
+<td>Саша:</td>
 <td><img src="poll.gif"
-width='<?php echo(100*round($no/($no+$yes),2)); ?>'
+width='<?php echo(100*round($Саша/($Саша+$Матвей+$Гера+$Коля),2)); ?>'
 height='20'>
-<?php echo(100*round($no/($no+$yes),2)); ?>%
+<?php echo(100*round($Саша/($Саша+$Матвей+$Гера+$Коля),2)); ?>%
+</td>
+</tr>
+<tr>
+<td>Гера:</td>
+<td><img src="poll.gif"
+width='<?php echo(100*round($Гера/($Саша+$Матвей+$Гера+$Коля),2)); ?>'
+height='20'>
+<?php echo(100*round($Гера/($Саша+$Матвей+$Гера+$Коля),2)); ?>%
+</td>
+</tr>
+<tr>
+<td>Коля:</td>
+<td><img src="poll.gif"
+width='<?php echo(100*round($Коля/($Саша+$Матвей+$Гера+$Коля),2)); ?>'
+height='20'>
+<?php echo(100*round($Коля/($Саша+$Матвей+$Гера+$Коля),2)); ?>%
 </td>
 </tr>
 </table>
